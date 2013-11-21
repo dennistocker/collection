b373375f132baab6df82401505619c95cf44975d
root <root@ubuntu.(none)>
2013-11-21 13:45:33 +0800

    change ase.py

--- python/aes.py
+++ python/aes.py
@@ -3,19 +3,24 @@
 import hashlib
 from Crypto.Cipher import AES
 
-BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-unpad = lambda s: s[0 : -ord(s[-1])]
-
 class AESCipher:
+    BS = 16
     def __init__(self, key):
         key = hashlib.md5(key).digest()
         self.cipher = AES.new(key)
 
+    def _pad(self, s):
+        pad = self.BS - len(s) % self.BS
+        return s + chr(pad) * pad
+
+    def _unpad(self, s):
+        pad = s[-1]
+        return s[:-ord(pad)]
+
     def encrypt(self, raw):
-        raw = pad(raw)
+        raw = self._pad(raw)
         return self.cipher.encrypt(raw)
 
     def decrypt(self, enc):
-        return unpad(self.cipher.decrypt(enc))
+        return self._unpad(self.cipher.decrypt(enc))
 
